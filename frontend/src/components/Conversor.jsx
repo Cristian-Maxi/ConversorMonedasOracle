a6a08d7f48703ea6ec560b79ba36fabfbe56bdb5
Cristian Gomez <134754887+Cristian-Maxi@users.noreply.github.com>
2024-04-10 20:58:26 -0300

    Update Conversor.jsx

--- frontend/src/components/Conversor.jsx
+++ frontend/src/components/Conversor.jsx
@@ -59,7 +59,7 @@ const Conversor = () => {
 
   const ConfirmarDescarga = () => {
     if(window.confirm("¿Decargar historial de las conversiones echas?")) {
-      window.location.href = 'https://conversormonedasoracle.onrender.com/descargar' // Si el usuario confirma, realizar la descarga
+      Axios.get("https://conversormonedasoracle.onrender.com/descargar") // Si el usuario confirma, realizar la descarga
     } 
   }
 
