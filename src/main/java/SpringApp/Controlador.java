dca0ec1c459de1cb57739d4a2f41fa4c4260128c
Cristian Gomez <134754887+Cristian-Maxi@users.noreply.github.com>
2024-04-10 21:42:10 -0300

    Update Controlador.java

--- src/main/java/SpringApp/Controlador.java
+++ src/main/java/SpringApp/Controlador.java
@@ -26,7 +26,8 @@ public class Controlador {
     }
     
     @GetMapping("/descargar")
-    public ResponseEntity<Resource> DescargarArchivo() {  
+    public ResponseEntity<Resource> DescargarArchivo() {
+        manejoArchivo.crearArchivo(nombreArchivo); 
         manejoArchivo.leerArchivo(nombreArchivo);
         Resource archivo = new FileSystemResource(nombreArchivo);
         return ResponseEntity.ok()
